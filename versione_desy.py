50595992b0c55f178781ac8078ef115157c24c26
Desy1803 <101573725+Desy1803@users.noreply.github.com>
2024-04-22 18:21:53 +0200

    Add files via upload

--- versione_desy.py
+++ versione_desy.py
@@ -1,81 +1,154 @@
+import sys
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-fcost = lambda x: 1
-tcost = (-1,1,2.5)
+minimo=float('inf')
 
-f1 = lambda x : (1/6)*x**6-(52/25)*x**5+(39/80)*x**4+(71/10)*x**3-(79/20)*x**2-x+(1/10)
-t1 = (-1.5, 11, 13870)
-f2 = lambda x : math.sin(x)+math.sin(10*x/3)
-t2 = (2.7, 7.5, 4.29)
-f3 = lambda x: sum(k*math.sin((k+1)*x+k) for k in range(1, 6))
-t3 = (-10, 10, 67)
-f4 = lambda x : -(16*x**2-24*x+5)*math.exp(-x)
-t4 = (1.9, 3.9, 3)
-f5 = lambda x : (3*x-1.4)*math.sin(18*x)
-t5 = (0, 1.2, 36)
-f6 = lambda x : -(x+math.sin(x))*math.exp(-(x**2))
-t6 = (-10, 10, 2.5)
-f7 = lambda x : math.sin(x)+math.sin(10*x/3)+math.log(x)-0.84*x+3
-t7 = (2.7, 7.5, 6)
-f8 = lambda x: sum(k*math.cos((k+1)*x+k) for k in range(1, 6))
-t8 = (-10, 10, 67)
-f9 = lambda x : math.sin(x)+math.sin(2*x/3)
-t9 = (3.1, 20.4, 1.7)
-f10 = lambda x : -x*math.sin(x)
-t10 = (0, 10, 11)
-f11 = lambda x : 2*math.cos(x)+math.cos(2*x)
-t11 = (-1.57, 6.28, 3)
-f12 = lambda x : (math.sin(x))**3+(math.cos(x))**3
-t12 = (0, 6.28, 2.2)
-f13 = lambda x : -(x**(2/3))+math.cbrt(x**2-1)
-t13 = (0.001, 0.99, 8.5)
-f14 = lambda x : -math.exp(-x)*math.sin(2*math.pi*x)
-t14 = (0, 4, 6.5)
-f15 = lambda x : (x**2-5*x+6)/(x**2+1)
-t15 = (-5, 5, 6.5)
-f16 = lambda x : 2*(x-3)**2+math.exp(1)**(0.5*x**2)
-t16 = (-3, 3, 85)
-f17 = lambda x : x**6-15*x**4+27*x**2+250
-t17 = (-4, 4, 2520)
-f18 = lambda x : (x-2)**2 if x<=3 else 2*math.log(x-2)+1
-t18 = (0, 6, 4)
-f19 = lambda x : -x+math.sin(3*x)-1
-t19 = (0, 6.5, 4)
-f20 = lambda x : (math.sin(x)-x)*math.exp(-(x**2))
-t20 = (-10, 10, 1.3)
+def selection_function(func_name):
+    global f
+    global t
+    if func_name == 'f1':
+        f = lambda x : (1/6)*x**6-(52/25)*x**5+(39/80)*x**4+(71/10)*x**3-(79/20)*x**2-x+(1/10)
+        t = (-1.5, 11, 13870)
+    elif func_name == 'f2':
+        f = lambda x : math.sin(x)+math.sin(10*x/3)
+        t = (2.7, 7.5, 4.29)
+    elif func_name == 'f3':
+        f = lambda x: sum(k*math.sin((k+1)*x+k) for k in range(1, 6))
+        t = (-10, 10, 67)
+    elif func_name == 'f4':
+        f = lambda x : -(16*x**2-24*x+5)*math.exp(-x)
+        t = (1.9, 3.9, 3)
+    elif func_name == 'f5':
+        f = lambda x : (3*x-1.4)*math.sin(18*x)
+        t = (0, 1.2, 36)
+    elif func_name == 'f6':
+        f = lambda x : -(x+math.sin(x))*math.exp(-(x**2))
+        t = (-10, 10, 2.5)
+    elif func_name == 'f7':
+        f = lambda x : math.sin(x)+math.sin(10*x/3)+math.log(x)-0.84*x+3
+        t = (2.7, 7.5, 6)
+    elif func_name == 'f8':
+        f = lambda x: sum(k*math.cos((k+1)*x+k) for k in range(1, 6))
+        t = (-10, 10, 67)
+    elif func_name == 'f9':
+        f = lambda x : math.sin(x)+math.sin(2*x/3)
+        t = (3.1, 20.4, 1.7)
+    elif func_name == 'f10':
+        f = lambda x : -x*math.sin(x)
+        t = (0, 10, 11)
+    elif func_name == 'f11':
+        f = lambda x : 2*math.cos(x)+math.cos(2*x)
+        t = (-1.57, 6.28, 3)
+    elif func_name == 'f12':
+        f = lambda x : (math.sin(x))**3+(math.cos(x))**3
+        t = (0, 6.28, 2.2)
+    elif func_name == 'f13':
+        f = lambda x : -(x**(2/3))+math.cbrt(x**2-1)
+        t = (0.001, 0.99, 8.5)
+    elif func_name == 'f14':
+        f = lambda x : -math.exp(-x)*math.sin(2*math.pi*x)
+        t = (0, 4, 6.5)
+    elif func_name == 'f15':
+        f = lambda x : (x**2-5*x+6)/(x**2+1)
+        t = (-5, 5, 6.5)
+    elif func_name == 'f16':
+        f = lambda x : 2*(x-3)**2+math.exp(1)**(0.5*x**2)
+        t = (-3, 3, 85)
+    elif func_name == 'f17':
+        f = lambda x : x**6-15*x**4+27*x**2+250
+        t = (-4, 4, 2520)
+    elif func_name == 'f18':
+        f = lambda x : (x-2)**2 if x<=3 else 2*math.log(x-2)+1
+        t = (0, 6, 4)
+    elif func_name == 'f19':
+        f = lambda x : -x+math.sin(3*x)-1
+        t = (0, 6.5, 4)
+    elif func_name == 'f20':
+        f = lambda x : (math.sin(x)-x)*math.exp(-(x**2))
+        t = (-10, 10, 1.3)
 
+def selection_function_np(func_name):
+    global fnp
+    global tnp
+    if func_name == 'f1':
+        fnp = lambda x: (1/6)*x**6-(52/25)*x**5+(39/80)*x**4+(71/10)*x**3-(79/20)*x**2-x+(1/10)
+        tnp = (-1.5, 11, 13870)
+    elif func_name == 'f2':
+        fnp = lambda x: np.sin(x)+np.sin(10*x/3)
+        tnp = (2.7, 7.5, 4.29)
+    elif func_name == 'f3':
+        fnp = lambda x: sum(k*np.sin((k+1)*x+k) for k in range(1, 6))
+        tnp = (-10, 10, 67)
+    elif func_name == 'f4':
+        fnp = lambda x: -(16*x**2-24*x+5)*np.exp(-x)
+        tnp = (1.9, 3.9, 3)
+    elif func_name == 'f5':
+        fnp = lambda x: (3*x-1.4)*np.sin(18*x)
+        tnp = (0, 1.2, 36)
+    elif func_name == 'f6':
+        fnp = lambda x: -(x+np.sin(x))*np.exp(-(x**2))
+        tnp = (-10, 10, 2.5)
+    elif func_name == 'f7':
+        fnp = lambda x: np.sin(x)+np.sin(10*x/3)+np.log(x)-0.84*x+3
+        tnp = (2.7, 7.5, 6)
+    elif func_name == 'f8':
+        fnp = lambda x: sum(k*np.cos((k+1)*x+k) for k in range(1, 6))
+        tnp = (-10, 10, 67)
+    elif func_name == 'f9':
+        fnp = lambda x: np.sin(x)+np.sin(2*x/3)
+        tnp = (3.1, 20.4, 1.7)
+    elif func_name == 'f10':
+        fnp = lambda x: -x*np.sin(x)
+        tnp = (0, 10, 11)
+    elif func_name == 'f11':
+        fnp = lambda x: 2*np.cos(x)+np.cos(2*x)
+        tnp = (-1.57, 6.28, 3)
+    elif func_name == 'f12':
+        fnp = lambda x: (np.sin(x))**3+(np.cos(x))**3
+        tnp = (0, 6.28, 2.2)
+    elif func_name == 'f13':
+        fnp = lambda x: -(x**(2/3))+np.cbrt(x**2-1)
+        tnp = (0.001, 0.99, 8.5)
+    elif func_name == 'f14':
+        fnp = lambda x: -np.exp(-x)*np.sin(2*np.pi*x)
+        tnp = (0, 4, 6.5)
+    elif func_name == 'f15':
+        fnp = lambda x: (x**2-5*x+6)/(x**2+1)
+        tnp = (-5, 5, 6.5)
+    elif func_name == 'f16':
+        fnp = lambda x: 2*(x-3)**2+np.exp(1)**(0.5*x**2)
+        tnp = (-3, 3, 85)
+    elif func_name == 'f17':
+        fnp = lambda x: x**6-15*x**4+27*x**2+250
+        tnp = (-4, 4, 2520)
+    elif func_name == 'f18':
+        fnp = lambda x: np.where(x <= 3, (x-2)**2, np.where(x > 2, 2*np.log(x-2)+1, np.nan))
+        tnp = (0, 6, 4)
+    elif func_name == 'f19':
+        fnp = lambda x: -x+np.sin(3*x)-1
+        tnp = (0, 6.5, 4)
+    elif func_name == 'f20':
+        fnp = lambda x: (np.sin(x)-x)*np.exp(-(x**2))
+        tnp = (-10, 10, 1.3)
 
-
-
-#---------------------------------------ALGORITMO--------------------------------------
-minimo = float('inf')
-a = 0
-b = 0
-L = 0
+#############################ALGORITMO###########################
 def fun(x):
-      y=[]
-      for i in range(len(x)):
-           y.append(fcost(x[i]))                          #RICORDA
-      return y
-
+    return fnp(x)
 
 def z(x):
     global minimo
-    val = fcost(x)                                        #RICORDA!
-    if(val < minimo):
+    val = f(x)
+    if val < minimo:
         minimo = val
     return val
-
-
+#                 CALCOLO R
 def calcola_R(x_interval, L):
     R = z(x_interval[0]) + z(x_interval[1]) - L * (x_interval[1] - x_interval[0]) / 2
-    #print(f"Calcolato R per intervallo {x_interval}: {round(R, 2)}")
     return R
-
-
+#                 TROVO MINR
 def trova_min_Ri(x_intervals, L):
     minimo1 = float('inf')
     minimo2 = float('inf')
@@ -100,81 +173,78 @@ def trova_min_Ri(x_intervals, L):
     
     return min_values
 
-
-
 def flatten(intervals):
-    # appiattisce array di intervalli in array di valori
-    if len(intervals)==0:
+    if len(intervals) == 0:
         return None
-    values = [intervals[0][0]]  # lower del primo intervallo
+    values = [intervals[0][0]]
     for interval in intervals:
-        values.append(interval[1])  # tutti gli upper
+        values.append(interval[1])
     return values
-
 
 def metodo_piyavskii_shubert(a, b, L, epsilon):
     global minimo
     k = 1
     maxIter = 12
-    x_intervals = [(a, b)]  # array di intervalli
+    x_intervals = [(a, b)]
     
     while True:
-        #print("Numero di iterazione:" , k)
+        #print("Iterazione \n ", k)
         interval2 = None
         min_values = trova_min_Ri(x_intervals, L)
+        #print("min_values: ", min_values)
         interval1 = x_intervals[min_values[0][0]] 
         xt_new1 = ((interval1[0] + interval1[1]) / 2) - ((z(interval1[1]) - z(interval1[0])) / (2 * L))
-        #print("xt1_new", xt_new1)
-        #print("x_intervals before:", x_intervals)
-        #print("min_values:", min_values)
-        # dal punto xt_new, crea due nuovi intervalli e rimuovi il precedente
-        if len(min_values)==1 and min_values[0][1]> minimo:
+        
+        if len(min_values) == 1 and min_values[0][1] > minimo:
+            print("qui")
             break
         x_intervals.remove(interval1)
         if min_values[0][1] < minimo:
             t = min_values[0][0]
             x_intervals.insert(t, (interval1[0], xt_new1))
             x_intervals.insert(t+1, (xt_new1, interval1[1]))
-
-        if len(min_values)>1 and len(x_intervals)>1:
-            #print("between",x_intervals)
+        #print("x_intervals between: ", x_intervals)
+        if len(min_values) > 1 and len(x_intervals) > 1:
             interval2 = x_intervals[min_values[1][0]]
             xt_new2 = ((interval2[0] + interval2[1]) / 2) - ((z(interval2[1]) - z(interval2[0])) / (2 * L))
-            #print("xt2_new", xt_new2)
-            if len(min_values)==1 and min_values[1][0]> minimo:
+            if len(min_values) == 1 and min_values[1][0] > minimo:
+                print("qui2")
                 break
             x_intervals.remove(interval2)
-            if min_values[1][1]< minimo:
+            if min_values[1][1] < minimo:
                 t = min_values[1][0]
                 x_intervals.insert(t, (interval2[0], xt_new2))
                 x_intervals.insert(t, (xt_new2, interval2[1]))
-        #print("x_intervals after:", x_intervals)
-        if interval1[1] - interval1[0] <= epsilon :
+        #print("x_intervals after: ", x_intervals)
+        if interval1[1] - interval1[0] <= epsilon:
             if interval2 != None and interval2[1] - interval2[0] <= epsilon :
                 x_values = flatten(x_intervals)
-                print("numero di iterazioni", k)
+                print("Numero di iterazioni con int2:", k)
                 return x_values, [z(x) for x in x_values]
-            if interval2 == None:
+                
+            if interval2 == None :
                 x_values = flatten(x_intervals)
-                print("numero di iterazioni", k)
+                print("Numero di iterazioni:", k)
                 return x_values, [z(x) for x in x_values]
 
         k += 1
-        
+    print("Numero di iterazioni fuori:", k)    
     x_values = flatten(x_intervals)
+    
     return x_values, [z(x) for x in x_values]
 
-def main():
+def main(func_name):
+    selection_function(func_name)
+    selection_function_np(func_name)
+    global t
     global minimo
-    a = tcost[0]
-    b = tcost[1]
-    L = tcost[2]
+    a = t[0]
+    b = t[1]
+    L = t[2]
     epsilon = 0.00001
-    print("precisione", epsilon)
     
-    # Esecuzione dell'algoritmo e plot dei punti trovati
     start_time = time.time()
-    x_values, z_values = metodo_piyavskii_shubert(a, b, L, epsilon)  # Assign return values here
+    x_values, z_values = metodo_piyavskii_shubert(a, b, L, epsilon)
     end_time = time.time()
     execution_time = end_time - start_time
 
@@ -184,34 +254,18 @@ def main():
     minimo_x = x_values[min_idx]
     print("x: ", minimo_x)
     print("y: ", minimo_z)
-    intervallo_valori_plot = np.linspace(a, b, 100000)
+    intervallo_valori_plot = np.linspace(a, b, 10000)
     y = fun(intervallo_valori_plot)
     
-    
     fig, ax = plt.subplots(figsize=(10, 6))
-    
-    # Plot della funzione obiettivo
     ax.plot(intervallo_valori_plot, y, color='blue', linestyle='-', label='Funzione')
-    
-    
-    # Plot dei punti trovati durante l'esecuzione dell'algoritmo
     ax.scatter(x_values, z_values, color='red', label='')
-    
-    # Plot del minimo globale
     ax.scatter(minimo_x, minimo_z, color='green', label='Minimo globale')
-    
-    # Impostazione del titolo e delle etichette degli assi
-    ax.set_title('Grafico ')
+    ax.set_title('Grafico')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    
-    # Mostra la legenda
     ax.legend()
-    
-    # Mostra il grafico
     plt.grid(True)
     plt.show()
-
- 
-
-main()
+    
+main("f19")
